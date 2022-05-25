07d935babe847b02a54c906a65d6b8658c88e032
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2022-05-25 10:31:01 +0900

    test

--- src/test/java/com/example/aicctvstream/AiCctvStreamApplicationTests.java
+++ src/test/java/com/example/aicctvstream/AiCctvStreamApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.aicctvstream;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AiCctvStreamApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.aicctvstream;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AiCctvStreamApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
