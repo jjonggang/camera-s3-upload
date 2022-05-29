a7cbdf343d4781e7e96a601355982306aee6659e
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2022-05-29 23:19:05 +0900

    보안작업

--- src/main/java/com/example/aicctvstream/webSocket/WebSocketConfig.java
+++ src/main/java/com/example/aicctvstream/webSocket/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/stream").setAllowedOrigins("*");
+        registry.addHandler(webSocketHandler, "/cctv/stream").setAllowedOrigins("*");
     }
 
 }
