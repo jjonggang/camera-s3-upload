0edb5ff7f824b24f5b0c46f1e8634b9c848466c2
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2022-05-30 12:24:29 +0900

    보안작업

--- src/main/java/com/example/aicctvstream/controller/cctv/CctvController.java
+++ src/main/java/com/example/aicctvstream/controller/cctv/CctvController.java
@@ -37,7 +37,7 @@ public class CctvController {
     public String streamUpload(@RequestParam("file") MultipartFile file) throws Exception{
         ImageFileResponseDto imageFileResponseDto = awsS3Service.upload2(file, "stream");
         webSocketHandler.sendMessageToAll(new TextMessage(imageFileResponseDto.getFileUrl()));
-        TimeUnit.MILLISECONDS.sleep(100);
+//        TimeUnit.MILLISECONDS.sleep(100);
 //        awsS3Service.deleteFile(imageFileResponseDto.getFileName());
         log.info("삭제완료");
         return imageFileResponseDto.getFileUrl();
